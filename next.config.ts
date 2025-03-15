9bd74ca34f418ac16b06412035b9452f074fac21
tatsuru koike <t-koike@marsflag.com>
2025-03-15 13:59:36 +0900

    first commit

---
+++ next.config.ts
@@ -0,0 +1,53 @@
+import type { NextConfig } from 'next';
+
+import initializeBundleAnalyzer from '@next/bundle-analyzer';
+
+// https://www.npmjs.com/package/@next/bundle-analyzer
+const withBundleAnalyzer = initializeBundleAnalyzer({
+  enabled: process.env.BUNDLE_ANALYZER_ENABLED === 'true'
+});
+
+// https://nextjs.org/docs/pages/api-reference/next-config-js
+const nextConfig: NextConfig = {
+  output: 'standalone',
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin'
+          }
+        ]
+      },
+      {
+        source: '/sw.js',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/javascript; charset=utf-8'
+          },
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self'"
+          }
+        ]
+      }
+    ];
+  }
+};
+
+export default withBundleAnalyzer(nextConfig);
