657e5bdeaa06e3a7a6547a72318f3eb4a06e5aaf
tatsuru koike <t-koike@marsflag.com>
2025-04-08 21:35:38 +0900

    アイコン画像を追加し、マニフェストファイルを更新して新しいアイコンサイズを反映しました。また、イベント型を新規作成しました。

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,15 +12,53 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: '#000000',
     icons: [
       {
-        purpose: 'maskable',
-        sizes: '512x512',
-        src: 'icon512_maskable.png',
+        src: 'icon-48x48.png',
+        sizes: '48x48',
         type: 'image/png'
       },
       {
-        purpose: 'any',
+        src: 'icon-72x72.png',
+        sizes: '72x72',
+        type: 'image/png'
+      },
+      {
+        src: 'icon-96x96.png',
+        sizes: '96x96',
+        type: 'image/png'
+      },
+      {
+        src: 'icon-128x128.png',
+        sizes: '128x128',
+        type: 'image/png'
+      },
+      {
+        src: 'icon-144x144.png',
+        sizes: '144x144',
+        type: 'image/png'
+      },
+      {
+        src: 'icon-152x152.png',
+        sizes: '152x152',
+        type: 'image/png'
+      },
+      {
+        src: 'icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: 'icon-256x256.png',
+        sizes: '256x256',
+        type: 'image/png'
+      },
+      {
+        src: 'icon-384x384.png',
+        sizes: '384x384',
+        type: 'image/png'
+      },
+      {
+        src: 'icon-512x512.png',
         sizes: '512x512',
-        src: 'icon512_rounded.png',
         type: 'image/png'
       }
     ],
---
+++ src/types/event.ts
@@ -0,0 +1,8 @@
+export type Event = {
+  id: string;
+  title: string;
+  start: string;
+  end: string;
+  all_day: boolean;
+  color: 'pink' | 'green';
+};
